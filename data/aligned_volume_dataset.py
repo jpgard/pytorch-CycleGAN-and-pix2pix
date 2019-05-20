9490031fc465483f3f64316a99d0e9b8437047fc
jpgard <joshua.patrick.gardner@gmail.com>
2019-05-20 13:09:55 -0700

    initial implementation of get_transform() for AlignedVolumeDataset using 3D spatial transforms

--- data/aligned_volume_dataset.py
+++ data/aligned_volume_dataset.py
@@ -1,17 +1,22 @@
 """
-
+A dataset of aligned volumes (3-D images).
 """
 
-from data.base_dataset import BaseDataset
 from data.image_folder import is_image_file
+from data.base_dataset import BaseDataset, get_params #, get_transform
+import util.spatial_transforms as spatial_transforms
 import os
 import re
 from collections import defaultdict
+import numpy as np
+from PIL import Image
+import torchvision.transforms as transforms
 
-def make_volume_dataset(dir, max_dataset_size=float("inf")) -> dict:
+
+def make_volume_dataset(dir, max_dataset_size=float("inf")):
     """
     Assemble a nested list of the slices in each observation.
-    Adapted from data.image_folder.make_dataset()
+    Adapted from data.image_folder.make_dataset().
     """
     images = []
     dataset_images = defaultdict(list)
@@ -34,7 +39,53 @@ def make_volume_dataset(dir, max_dataset_size=float("inf")) -> dict:
         assert len(dataset_images[dataset_id]) == z + 1, "missing z-slice in input data"
     if len(dataset_images) > max_dataset_size:
         raise NotImplementedError
-    return dataset_images
+    # return just a list of the z-slice filepaths for each dataset
+    return list(dataset_images.items())
+
+
+def get_transform(opt, params=None, grayscale=False, method=Image.BICUBIC, convert=True):
+    """
+    Get the 3d versions of transforms; modified from base_dataset.get_transform().
+    """
+    transform_list = []
+    if grayscale:
+        transform_list.append(transforms.Grayscale(1))
+    if 'resize' in opt.preprocess:
+        osize = [opt.load_size, opt.load_size]
+        transform_list.append(transforms.Resize(osize, method))
+    elif 'scale_width' in opt.preprocess:
+        transform_list.append(transforms.Lambda(lambda img: __scale_width(img, opt.load_size, method)))
+
+    if 'crop' in opt.preprocess:
+        if params is None:
+            # perform random crop with scale of 1
+            transform_list.append(spatial_transforms.MultiScaleRandomCrop(scales=(1,), size=opt.crop_size))
+            # transform_list.append(transforms.RandomCrop(opt.crop_size))
+        else:
+            raise NotImplementedError
+            # transform_list.append(transforms.Lambda(lambda img: __crop(img, params['crop_pos'], opt.crop_size)))
+
+    if opt.preprocess == 'none':
+        raise NotImplementedError
+    #     transform_list.append(transforms.Lambda(lambda img: __make_power_2(img, base=4, method=method)))
+
+    if not opt.no_flip:
+        raise NotImplementedError
+        # if params is None:
+        #     transform_list.append(transforms.RandomHorizontalFlip())
+        # elif params['flip']:
+        #     transform_list.append(transforms.Lambda(lambda img: __flip(img, params['flip'])))
+
+    if convert:
+        transform_list += [spatial_transforms.ToTensor()]
+        # transform_list += [transforms.ToTensor()]
+        if grayscale:
+            transform_list += [spatial_transforms.Normalize((0.5,), (0.5,))]
+            # transform_list += [transforms.Normalize((0.5,), (0.5,))]
+        else:
+            transform_list += [spatial_transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+            # transform_list += [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    return transforms.Compose(transform_list)
 
 
 class AlignedVolumeDataset(BaseDataset):
@@ -48,27 +99,46 @@ class AlignedVolumeDataset(BaseDataset):
 
 
     def __len__(self):
-        # todo
-        pass
+        return len(self.AB_paths)
 
-    def __getitem__(self, item):
-    """Return a data point and its metadata information.
+    def __getitem__(self, index):
+        """Return a data point and its metadata information.
 
-    Parameters:
-        index - - a random integer for data indexing
+        Parameters:
+            index - - a random integer for data indexing
 
-    Returns a dictionary that contains A, B, A_paths and B_paths
-        A (tensor) - - an image in the input domain
-        B (tensor) - - its corresponding image in the target domain
-        A_paths (str) - - image paths
-        B_paths (str) - - image paths (same as A_paths)
-    """
-        # todo
-        pass
+        Returns a dictionary that contains A, B, A_paths and B_paths
+            A (tensor) - - an image in the input domain
+            B (tensor) - - its corresponding image in the target domain
+            A_paths (str) - - image paths
+            B_paths (str) - - image paths (same as A_paths)
+        """
+        # read a image given a random integer index
+        AB_slice_paths = self.AB_paths[index]
+        A_slices = list()
+        B_slices = list()
+        for s in AB_slice_paths:
+            AB = Image.open(AB_slice_paths).convert('RGB')
+            # split AB image into A and B
+            w, h = AB.size
+            w2 = int(w / 2)
+            A = AB.crop((0, 0, w2, h))
+            B = AB.crop((w2, 0, w, h))
+            A_slices.append(A)
+            B_slices.append(B)
+        # todo: check that this is correct concatenation axis for pytorch; should be channels-first?
+        A = np.concatenate(A_slices, axis=0)
+        B = np.concatenate(B_slices, axis=0)
+        # apply the same transform to both A and B
+        transform_params = get_params(self.opt, A.size)
+        A_transform = get_transform(self.opt, transform_params, grayscale=(self.input_nc == 1))
+        B_transform = get_transform(self.opt, transform_params, grayscale=(self.output_nc == 1))
+        # todo: check that transform is applied correctly to 3d images
+        A = A_transform(A)
+        B = B_transform(B)
 
-    def modify_commandline_options(parser, is_train):
-        # todo
-        pass
+        return {'A': A, 'B': B, 'A_paths': AB_slice_paths, 'B_paths': AB_slice_paths}
+
 
 
 
